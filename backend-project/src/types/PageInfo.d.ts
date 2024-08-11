1261219904cd97afb1470dec023c32e52e85ea48
PenguinRoger <koonmax555@gmail.com>
2024-08-11 12:52:00 +0700

    set up system backend

---
+++ backend-project/src/types/PageInfo.d.ts
@@ -0,0 +1,11 @@
+export interface PageInfoResponse {
+    total:number
+    offset:number
+    pageSize:number
+}
+
+export interface PageInfoRequest {
+    offset:string,
+    pageSize:string,
+}
+
